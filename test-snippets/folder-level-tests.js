cee0b4ac3d06b654040338027e0e3a2d231cbfd6
xin-217 <61130306+xin-217@users.noreply.github.com>
2022-11-18 10:45:41 +0800

    Update folder-level-tests.js

--- test-snippets/folder-level-tests.js
+++ test-snippets/folder-level-tests.js
@@ -1,8 +1,8 @@
   pm.test("FOLDER Status code is 200", function () {
       pm.response.to.have.status(200);
     });
-    pm.test("FOLDER Response time is less than 500ms", function () {
-        pm.expect(pm.response.responseTime).to.be.below(500);
+    pm.test("FOLDER Response time is less than 1000ms", function () {
+        pm.expect(pm.response.responseTime).to.be.below(1000);
     });
     pm.test("FOLDER Content-Type is present", function () {
         pm.response.to.have.header("Content-Type");
