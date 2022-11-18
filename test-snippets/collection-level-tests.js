95b32b64d6f36e6ec02b17ce07758787555abf82
xin-217 <61130306+xin-217@users.noreply.github.com>
2022-11-18 15:10:04 +0800

    Update collection-level-tests.js

--- test-snippets/collection-level-tests.js
+++ test-snippets/collection-level-tests.js
@@ -1,8 +1,8 @@
     pm.test("COLLECTION Status code is 200", function () {
       pm.response.to.have.status(200);
     });
-    pm.test("COLLECTION Response time is less than 1500ms", function () {
-        pm.expect(pm.response.responseTime).to.be.below(1500);
+    pm.test("COLLECTION Response time is less than 1800ms", function () {
+        pm.expect(pm.response.responseTime).to.be.below(1800);
     });
     pm.test("COLLECTION Content-Type is present", function () {
         pm.response.to.have.header("Content-Type");
